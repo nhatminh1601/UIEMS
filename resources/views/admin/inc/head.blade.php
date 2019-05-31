1a772fb295d30e0379427df55e7618f964ccd077
minhnguyen <nguyentrannhatminh@gmail.com>
2019-05-31 14:39:39 +0700

    - upload css

--- resources/views/admin/inc/head.blade.php
+++ resources/views/admin/inc/head.blade.php
@@ -4,8 +4,15 @@
     <title>AdminLTE 2 | Dashboard</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="dist/select2.min.css">
+    <!-- Bootstrap time Picker -->
+    <link rel="stylesheet" href="dist/bootstrap-timepicker.min.css">
     <link rel="stylesheet" href="dist/adminlte.min.css">
-
+    <link rel="stylesheet" href="css/bootstrap-table.min.css">
+    <link rel="stylesheet" href="css/manager-user.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
     <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
